85927d92a2c96ec334fdd724c2fc3e2031092913
ABDUL WAHAB IDDRIS <abdulwahabiddris08@gmail.com>
2024-04-13 19:54:43 +0000

    i

--- app/statistics/page.jsx
+++ app/statistics/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react';
+import React, { useEffect , useState } from 'react';
 import Navigation from "@/components/Navigation";
 import MainContent from "@/components/content";
 import Grid from "funuicss/ui/grid/Grid";
@@ -10,16 +10,52 @@ import data from "@/components/data";
 import Section from "funuicss/ui/specials/Section";
 import Table from "funuicss/ui/table/Table";
 import dynamic from "next/dynamic";
+import Axios  from 'axios';
+import { URI } from '@/functions/endpoint';
 const MainChart = dynamic(()=>import("@/components/MainGraph") ,{ssr:false})
 const Chart = dynamic(()=>import("@/components/Graph") ,{ssr:false})
 const GraphChart = dynamic(() => import("@/components/RangeGraph"), { ssr: false })
-
+import Input from 'funuicss/ui/input/Input' 
+import RowFlex from 'funuicss/ui/specials/RowFlex'
+import { PiEye } from 'react-icons/pi';
+import Circle from "funuicss/ui/specials/Circle"
 const Statistics = ()=>{
 const data_table = {
     "data":[] ,
     "titles":["Title One","Title Two","Title Three","Title Four","Title Five","Title"],
 }
 
+const [outlier_data, setoutlier_data] = useState(null) 
+
+const GetOutlier = (req) => {
+    Axios.get(URI + `/outlier/${req.year}/${req.month}`)
+    .then(res => {
+        let getDocs , data  
+        getDocs = res.data 
+        data = {
+            "data": getDocs,
+            "titles": ["Firm ID", "Establishment", "Interviewer", "Product ID", "%", "Month Price" , "View"],
+            "fields": ["firm_id", "establishment_name", 'interviewer_name' , "firm_product_id", "percent_change", "current_monthly_price"],
+          }
+        setoutlier_data(data)
+    })
+}
+useEffect(() => {
+    GetOutlier({'year' : 2023 ,  'month' : 12})
+}, [])
+
+const HandleOutlierQuery = (year_month) => {
+    let year , month 
+    year = year_month.slice(0 , year_month.indexOf("-")) 
+    month = year_month.slice(-2) 
+   
+    new Promise((resolve, reject) => {
+        setoutlier_data(null)
+        resolve()
+    })
+    .then(res =>  GetOutlier({'year' : year ,  'month' : month}))
+
+}
 return (
         <div>
             <Navigation title={"Statistics"} />
@@ -65,15 +101,53 @@ return (
 
                 <Section gap={2} />
                 <Card
+                style={{gap:0}}
                     header={<div className={"padding bb"}>
-                        <Text text={"Data"} block/>
+                        <RowFlex gap={1} justify="space-between">
+                            <div>
+                            <Text text={"Data"} block/>
                         <Text heading={"h3"} text={"Table"} bold block/>
+                            </div>
+                            <div>
+                                <div>
+                                    <Text 
+                                    text="Month/Year*"
+                                    size="small"
+                                    bold 
+                                    color="dark200"
+                                    />
+                                </div>
+                                <Input
+                                type='month'
+                                bordered
+                                onChange={(e) => HandleOutlierQuery(e.target.value)}
+                                />
+                            </div>
+                        </RowFlex>
                     </div>}
                     funcss='roundEdge'
                     xl
                     body={
                         <div className={""}>
-                            <Table data={data_table}  funcss={"text-small"}/>
+                          {
+                            outlier_data ?
+                            <Table data={outlier_data}  funcss={"text-small"} pageSize={20}
+                              customColumns={[
+                    {
+                      title: 'Actions',
+                      render: (data) => (
+                        <Circle bg='primary' size={1.5} onClick={() => {
+
+                        }}>
+                          <PiEye />
+                        </Circle>
+                      ),
+                    },
+                  ]}  
+                            
+                            />
+                            : <div className='height-400 dark600 skeleton  padding-50' />
+                          }
                         </div>
                     }
 
